92d1bbe72a0a8fa6056b6b0578cf78a613582e82
Illia <illiadovho@gmail.com>
2025-11-14 16:47:05 +0000

    Added index

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.js";
+import decksRoutes from "./routes/decks.js";
+import "./db-init.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/health", (req, res) => {
+    res.json({ status: "ok" });
+});
+
+app.use("/api", authRoutes);
+app.use("/api", decksRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
